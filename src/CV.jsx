f5da5fbd6fc817995835fc850454c1c598fce29c
BenerdAndrian <ben527466@gmail.com>
2025-05-05 07:12:41 +0000

    fixing error in downloadPDF function

--- src/CV.jsx
+++ src/CV.jsx
@@ -42,10 +42,26 @@ function GenerateCV(props){
         }).then((canvas) => {
             const imgData = canvas.toDataURL("image/png");
             const pdf = new jsPDF("p", "mm", "a4");
-            const pdfWidth = pdf.internal.pageSize.getWidth();
-            const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
     
-            pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdf.internal.pageSize.getHeight();
+    
+            const imgWidth = pdfWidth;
+            const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    
+            let heightLeft = imgHeight;
+            let position = 0;
+    
+            pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+            heightLeft -= pdfHeight;
+    
+            while (heightLeft > 0) {
+                position = heightLeft - imgHeight;
+                pdf.addPage();
+                pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+                heightLeft -= pdfHeight;
+            }
+    
             pdf.save("CV.pdf");
         });
     };
